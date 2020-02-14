441b42c22edb09c0ec7837b54d4bfcd82cba3ff6
Sangeeth Dev <discoversangeeth@gmail.com>
2020-02-14 11:33:23 +0530

    added still hungry print stmt

--- hungry.py
+++ hungry.py
@@ -1,6 +1,9 @@
+#git diff command
 hungry = input("Are you hungry?")
 if(hungry == 'yes'):
     print('Eat food then')
 else:
     print('Do your homework')
+    
+print("I'm still hungry for knowledge")
     
